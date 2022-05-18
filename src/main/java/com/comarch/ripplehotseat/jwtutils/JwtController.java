dc427b004de95d48b82231998a2d1eb88d85e043
Krzysztof Sajkowski <248864@student.pwr.edu.pl>
2022-05-18 12:02:01 +0200

    Added @CrossOrigin adnotation

--- src/main/java/com/comarch/ripplehotseat/jwtutils/JwtController.java
+++ src/main/java/com/comarch/ripplehotseat/jwtutils/JwtController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("https://ripple-hot-seat-backend-app.herokuapp.com")
 public class JwtController {
 
 	@Autowired
