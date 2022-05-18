a4cdbda4d3cf42119a0d7032a1b7e4ac99e52e89
Squareesque <64011762+Squareesque@users.noreply.github.com>
2022-05-18 12:07:32 +0200

    Update UserRestController.java

--- src/main/java/com/comarch/ripplehotseat/controller/UserRestController.java
+++ src/main/java/com/comarch/ripplehotseat/controller/UserRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -25,6 +26,7 @@ import com.comarch.ripplehotseat.util.ObjectMapperUtils;
  *
  */
 @RestController
+@CrossOrigin("https://ripple-hot-seat-backend-app.herokuapp.com")
 @RequestMapping("/users")
 public class UserRestController {
 
