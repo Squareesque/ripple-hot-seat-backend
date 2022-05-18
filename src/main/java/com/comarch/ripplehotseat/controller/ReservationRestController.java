101ed160d5c23c897631383c6dd16b5769baec3e
Squareesque <64011762+Squareesque@users.noreply.github.com>
2022-05-18 12:08:23 +0200

    Update ReservationRestController.java

--- src/main/java/com/comarch/ripplehotseat/controller/ReservationRestController.java
+++ src/main/java/com/comarch/ripplehotseat/controller/ReservationRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,6 +29,7 @@ import com.comarch.ripplehotseat.util.ObjectMapperUtils;
  *
  */
 @RestController
+@CrossOrigin("https://ripple-hot-seat-backend-app.herokuapp.com")
 @RequestMapping("/reservations")
 public class ReservationRestController {
 
