7b1fc3577db424a6ff08644d4789a2e024027379
Krzysztof Sajkowski <248864@student.pwr.edu.pl>
2022-06-21 22:29:13 +0200

    Added percentage request

---
+++ src/main/java/com/comarch/ripplehotseat/dto/PercentageDTO.java
@@ -0,0 +1,25 @@
+package com.comarch.ripplehotseat.dto;
+
+public class PercentageDTO {
+
+	private String roomId;
+	private int percentage;
+	
+	public String getRoomId() {
+		return roomId;
+	}
+	
+	public void setRoomId(String roomId) {
+		this.roomId = roomId;
+	}
+
+	public int getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(int percentage) {
+		this.percentage = percentage;
+	}
+	
+	
+}
